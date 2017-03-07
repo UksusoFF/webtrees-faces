1bac254994e0e145adf8af7c1d9cdeac9f282d32
Uksusov Kirill <uksusoff@yandex.ru>
2017-03-07 22:52:26 +0400

    Replace css to less.

--- module.php
+++ module.php
@@ -17,7 +17,7 @@ use UksusoFF\WebtreesModules\PhotoNoteWithImageMap\Helpers\JsonResponseHelper as
 
 class PhotoNoteWithImageMap extends AbstractModule implements ModuleMenuInterface, ModuleConfigInterface
 {
-    const CUSTOM_VERSION = '2.1.5';
+    const CUSTOM_VERSION = '2.1.6';
     const CUSTOM_WEBSITE = 'https://github.com/UksusoFF/photo_note_with_image_map';
 
     var $directory;
