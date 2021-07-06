ef28266084c46e89bd0c1bccc7e73b3cac03e002
z-yurets <zaharchenko.yurij@gmail.com>
2021-07-06 23:13:04 +0300

    Create ukrainian translate
    Add Ukrainian language

---
+++ resources/langs/uk.php
@@ -0,0 +1,46 @@
+<?php
+
+// Note: Commented keys already exist in webtrees translations.
+
+use Fisharebest\Webtrees\I18N;
+
+return [
+    // Frontend: Modals
+    'Enter individuals id or something else' => 'Введіть id особи, або щось інше',
+    'This operation can not be undone' => 'Цю операцію не можна скасувати',
+    'You have enabled "Create links" feature' => 'Ви ввімкнули функцію "Створювати зв'язки"',
+    'Linked individuals will not be detached from media' => 'Пов’язаних осіб не буде відірвано від медіа файлів',
+    'Are you sure that want delete %s from image?' => 'Ви насправді хочете видалити %s з зображення?',
+    // Config: JavaScript
+    'Are you sure?' => 'Ви впевнені?',
+    'Read more' => 'Читати ще',
+    // Config
+    'Reset filters' => 'Скинути фільтри',
+    // Config: Missed
+    'Missed' => 'Втрачені',
+    // 'Remove' => 'Видалити',
+    'Remove all records without media' => 'Видалити всі записи без медіа',
+    'Find' => 'Знайти',
+    'Try to find missed records by filename' => 'Спробувати знайти втрачені записи за іменем файлу',
+    // Config: Settings
+    'Settings' => 'Налаштування',
+    'Read XMP data' => 'Читати XMP дані',
+    'Read and show XMP data (such as Goggle Picasa face tags) from media file' => 'Читати і показувати XMP дані (наприклад, відмітки обличчя в Goggle Picasa) із медіа файлу',
+    'Create links' => 'Створити зв'язки',
+    'Link individual with media when mark them on photo' => 'Пов'язувати особу з медіа при додаванні її на фото',
+    'Show meta' => 'Відображати мета-дані',
+    'Load and show information from linked fact' => 'Завантажувати та відображати інформацію з пов'язаного факту',
+    'Show tab' => 'Відображати вкладку',
+    'Show tab on individuals page' => 'Відображати вкладку на сторінці особи',
+    // Config: Table
+    // 'Media' => 'Медіа',
+    // 'Notes' => 'Примітки',
+    // 'Status' => 'Статус',
+    'Actions' => 'Дії',
+    // Config: Messages
+    '%s record' . I18N::PLURAL . '%s records' => '%s запис' . I18N::PLURAL . '%s запису' . I18N::PLURAL . '%s записів',
+    'has been deleted' . I18N::PLURAL . 'have been deleted' => 'була видалена' . I18N::PLURAL . 'було видалене' . I18N::PLURAL . 'було видалене',
+    'has been repaired' . I18N::PLURAL . 'have been repaired' => 'була відновлена' . I18N::PLURAL . 'було відновлене'. 'було відновлене',
+    // 'Enabled' => 'Увімкнено',
+    'Disabled' => 'Вимкнено',
+];
