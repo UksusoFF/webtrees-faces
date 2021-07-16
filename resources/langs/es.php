287647bcf4f8030980e8b5c81604d571a8bf7ce9
yako1984 <67491705+yako1984@users.noreply.github.com>
2021-07-16 20:46:26 +0200

    Add languaje spanish

---
+++ resources/langs/es.php
@@ -0,0 +1,46 @@
+<?php
+
+// Note: Commented keys already exist in webtrees translations.
+
+use Fisharebest\Webtrees\I18N;
+
+return [
+    // Frontend: Modals
+    'Enter individuals id or something else' => 'Ingrese la identificación de las personas u otra cosa',
+    'This operation can not be undone' => 'Esta operación no se puede deshacer',
+    'You have enabled "Create links" feature' => 'Ha habilitado la función "Crear enlaces"',
+    'Linked individuals will not be detached from media' => 'Las personas vinculadas no se separarán de los archivos multimedia',
+    'Are you sure that want delete %s from image?' => '¿Estás seguro de que quieres elimina-r a %s de la imagen?',
+    // Config: JavaScript
+    'Are you sure?' => 'Estás seguro?',
+    'Read more' => 'Leer más',
+    // Config
+    'Reset filters' => 'Restablecer filtros',
+    // Config: Missed
+    'Missed' => 'Omitido',
+    // 'Remove' => 'Eliminar',
+    'Remove all records without media' => 'Eliminar todos los registros sin multimedia',
+    'Find' => 'Buscar',
+    'Try to find missed records by filename' => 'Intente encontrar registros perdidos por nombre de archivo',
+    // Config: Settings
+    'Settings' => 'Configuración',
+    'Read XMP data' => 'Leer datos XMP',
+    'Read and show XMP data (such as Goggle Picasa face tags) from media file' => 'Leer y mostrar datos XMP (como las etiquetas faciales de Goggle Picasa) del archivo multimedia',
+    'Create links' => 'Crear enlaces',
+    'Link individual with media when mark them on photo' => 'Vincular a la persona con el archivo multimedia cuando los marque en la foto',
+    'Show meta' => 'Mostrar metadatos',
+    'Load and show information from linked fact' => 'Cargar y mostrar información de hechos vinculados',
+    'Show tab' => 'Mostrar pestaña',
+    'Show tab on individuals page' => 'Mostrar pestaña en la pagina personas',
+    // Config: Table
+    // 'Media' => 'Media',
+    // 'Notes' => 'Notas',
+    // 'Status' => 'Estado',
+    'Actions' => 'Acciones',
+    // Config: Messages
+    '%s record' . I18N::PLURAL . '%s records' => '%s registrado' . I18N::PLURAL . '%s registrados',
+    'has been deleted' . I18N::PLURAL . 'have been deleted' => 'ha sido eliminados' . I18N::PLURAL . 'han sido eliminados',
+    'has been repaired' . I18N::PLURAL . 'have been repaired' => 'ha sido reparado' . I18N::PLURAL . 'han sido reparados',
+    // 'Enabled' => 'Activado',
+    'Disabled' => 'Desactivado',
+];
