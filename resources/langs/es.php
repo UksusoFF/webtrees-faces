49ede8e315cdea330af97880cd10170de71f7095
yako1984 <67491705+yako1984@users.noreply.github.com>
2021-07-16 21:09:05 +0200

    Update es.php

--- resources/langs/es.php
+++ resources/langs/es.php
@@ -8,9 +8,9 @@ return [
     // Frontend: Modals
     'Enter individuals id or something else' => 'Ingrese la identificación de las personas u otra cosa',
     'This operation can not be undone' => 'Esta operación no se puede deshacer',
-    'You have enabled "Create links" feature' => 'Ha habilitado la función "Crear enlaces"',
+    'You have enabled "Create links" feature' => 'Ha habilitado la función "Crear vinculos"',
     'Linked individuals will not be detached from media' => 'Las personas vinculadas no se separarán de los archivos multimedia',
-    'Are you sure that want delete %s from image?' => '¿Estás seguro de que quieres elimina-r a %s de la imagen?',
+    'Are you sure that want delete %s from image?' => '¿Estás seguro de que quieres eliminar a %s de la imagen?',
     // Config: JavaScript
     'Are you sure?' => 'Estás seguro?',
     'Read more' => 'Leer más',
@@ -21,12 +21,12 @@ return [
     // 'Remove' => 'Eliminar',
     'Remove all records without media' => 'Eliminar todos los registros sin multimedia',
     'Find' => 'Buscar',
-    'Try to find missed records by filename' => 'Intente encontrar registros perdidos por nombre de archivo',
+    'Try to find missed records by filename' => 'Intente encontrar registros perdidos por el nombre del archivo',
     // Config: Settings
     'Settings' => 'Configuración',
     'Read XMP data' => 'Leer datos XMP',
     'Read and show XMP data (such as Goggle Picasa face tags) from media file' => 'Leer y mostrar datos XMP (como las etiquetas faciales de Goggle Picasa) del archivo multimedia',
-    'Create links' => 'Crear enlaces',
+    'Create links' => 'Crear vinculos',
     'Link individual with media when mark them on photo' => 'Vincular a la persona con el archivo multimedia cuando los etiquetes en las fotos',
     'Show meta' => 'Mostrar metadatos',
     'Load and show information from linked fact' => 'Cargar y mostrar información de hechos vinculados',
@@ -38,7 +38,7 @@ return [
     // 'Status' => 'Estado',
     'Actions' => 'Acciones',
     // Config: Messages
-    '%s record' . I18N::PLURAL . '%s records' => '%s registrado' . I18N::PLURAL . '%s registrados',
+    '%s record' . I18N::PLURAL . '%s records' => '%s registro' . I18N::PLURAL . '%s registros',
     'has been deleted' . I18N::PLURAL . 'have been deleted' => 'ha sido eliminados' . I18N::PLURAL . 'han sido eliminados',
     'has been repaired' . I18N::PLURAL . 'have been repaired' => 'ha sido reparado' . I18N::PLURAL . 'han sido reparados',
     // 'Enabled' => 'Activado',
