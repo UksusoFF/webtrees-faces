efe96a061836a7b29d2d825455733c9a1275c8b9
yako1984 <67491705+yako1984@users.noreply.github.com>
2021-07-18 04:16:45 +0200

    Update es.php

--- resources/langs/es.php
+++ resources/langs/es.php
@@ -5,7 +5,7 @@
 use Fisharebest\Webtrees\I18N;
 
 return [
-    'Faces' => 'Faces',
+    'Faces' => 'Etiquetar Fotos',
     // Frontend: Modals
     'Enter individuals id or something else' => 'Ingresa el nombre de la persona a etiquetar o un texto descriptivo',
     'This operation can not be undone' => 'Esta operación no se puede deshacer',
