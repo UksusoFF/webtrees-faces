95c79e8ecf3881dec05b54d174d74a4a9551c740
Gudjon Sigurdsson <100283838+gudjonsi@users.noreply.github.com>
2024-12-02 17:49:49 +0000

    Create is.php
    Created Icelandic language file for webtrees-faces.

---
+++ resources/langs/is.php
@@ -0,0 +1,48 @@
+<?php
+
+// Note: Commented keys already exist in webtrees translations.
+
+use Fisharebest\Webtrees\I18N;
+
+return [
+    'Faces' => 'Andlit',
+    // Frontend: Modals
+    'Enter individuals id or something else' => 'Sláðu inn persónuauðkenni eða eitthvað annað',
+    'This operation can not be undone' => 'Ekki er hægt að afturkalla þessa aðgerð',
+    'You have enabled "Create links" feature' => 'Þú hefur virkjað eiginleikann „Búa til tengla“',
+    'Linked individuals will not be detached from media' => 'Tengdir einstaklingar verða ekki aðskildir frá gögnum',
+    'Are you sure that want delete %s from image?' => 'Ertu viss um að þú viljir eyða %s frá mynd?',
+    // Config: JavaScript
+    'Are you sure?' => 'Ertu viss?',
+    'Read more' => 'Lesa meira',
+    // Config
+    'Reset filters' => 'Endurstilla síur',
+    // Config: Missed
+    'Missed' => 'Tapað',
+    // 'Remove' => 'Remove',
+    'Remove all records without media' => 'Fjarlægðu allar skrár án gagna',
+    'Find' => 'Find',
+    'Try to find missed records by filename' => 'Reyndu að finna töpuð gögn eftir skráarnafni',
+    // Config: Settings
+    'Settings' => 'Stillingar',
+    'Read XMP data' => 'Lesa XMP lýsigögn',
+    'Read and show XMP data (such as Goggle Picasa face tags) from media file' => 'Lestu og sýndu XMP lýsigögn (eins og Goggle Picasa andlitsmerki) úr gagnaskrá',
+    'Create links' => 'Búðu til tengla',
+    'Link individual with media when mark them on photo' => 'Tengdu einstakling við gögn þegar þú merkir hann á mynd',
+    'Show meta' => 'Sýna lýsigögn',
+    'Load and show information from linked fact' => 'Hlaða og sýna upplýsingar frá tengdri staðreynd',
+    'Show tab' => 'Sýna flipa',
+    'Show tab on individuals page' => 'Sýna flipa á síðu einstaklinga',
+    // Config: Table
+    // 'Media' => 'Media',
+    // 'Notes' => 'Notes',
+    // 'Status' => 'Status',
+    'Actions' => 'Aðgerðir',
+    // Config: Messages
+    '%s record' . I18N::PLURAL . '%s records' => '%s færsla' . I18N::PLURAL . '%s færslur',
+    'has been deleted' . I18N::PLURAL . 'have been deleted' => 'hefur verið eytt' . I18N::PLURAL . 'hefur verið eytt',
+    'has been repaired' . I18N::PLURAL . 'have been repaired' => 'hefur verið lagfærð' . I18N::PLURAL . 'hafa verið lagfærðar',
+    // 'Enabled' => 'Enabled',
+    'Disabled' => 'Afvirkjað',
+    'Highlight all' => 'Áherslumerktu allt',
+];
